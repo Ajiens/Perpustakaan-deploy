70441994811b08e79608a692c3257620d95c0a48
mahirairgn <mahirairgn@gmail.com>
2023-10-28 21:43:26 +0700

    memperbaiki tampilan wishlist

--- add_wishlist/urls.py
+++ add_wishlist/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
     path("", wishlist_view, name='wishlist'),
     path("add/<int:book_id>/", add_to_wishlist, name="add"),
     path("remove/<int:book_id>/", remove_from_wishlist, name="remove"),
+    path('get-books/', get_books_json, name='get_books_json'),
 ]
--- add_wishlist/views.py
+++ add_wishlist/views.py
@@ -1,33 +1,25 @@
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
-from django.contrib import messages
 from django.urls import reverse
 from book.models import Book
+from django.core import serializers
 
 def add_to_wishlist(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     wishlist = request.session.get('wishlist', [])
-    
     if book.id not in wishlist:
         wishlist.append(book.id)
         request.session['wishlist'] = wishlist
         success = True
     else:
         success = False
-    
     return JsonResponse({'success': success})
 
 
 def remove_from_wishlist(request, book_id):
     wishlist = request.session.get('wishlist', [])
-    
-    if book_id in wishlist:
-        wishlist.remove(book_id)
-        request.session['wishlist'] = wishlist
-        success = True
-    else:
-        success = False
-
+    wishlist.remove(book_id)
+    request.session['wishlist'] = wishlist
     return HttpResponseRedirect(reverse('wishlist'))
 
 
@@ -35,3 +27,10 @@ def wishlist_view(request):
     wishlist = request.session.get('wishlist', [])
     books = Book.objects.filter(pk__in=wishlist)
     return render(request, 'wishlist.html', {'wishlist': books})
+
+
+def get_books_json(request):
+    wishlist = request.session.get('wishlist', [])
+    books = Book.objects.filter(pk__in=wishlist)  # Filter hanya buku yang ada dalam wishlist
+    book_data = serializers.serialize('json', books)
+    return JsonResponse(book_data, safe=False)
