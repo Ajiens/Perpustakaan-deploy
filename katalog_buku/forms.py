538dde9b3e51d22b697a2e0ad396790d9b1fb697
widyaprms <arini.widya@ui.ac.id>
2023-10-28 16:40:14 +0700

    Membuat halaman add new book

--- katalog_buku/forms.py
+++ katalog_buku/forms.py
@@ -4,4 +4,4 @@ from katalog_buku.models import Book
 class BookForm(ModelForm):
     class Meta:
         model = Book
-        fields = ["title", "author", "cover_link", "average_rating", "harga"]
+        fields = ["title", "author", "cover_link", "harga"]
