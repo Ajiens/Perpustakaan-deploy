ad001089957ddf0bcb518ea39c231f171b43d2b4
mahirairgn <mahirairgn@gmail.com>
2023-10-28 15:54:43 +0700

    memperbaiki tampilan wishlist

--- add_wishlist/views.py
+++ add_wishlist/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.contrib import messages
 from django.urls import reverse
@@ -11,13 +11,27 @@ def add_to_wishlist(request, book_id):
     if book.id not in wishlist:
         wishlist.append(book.id)
         request.session['wishlist'] = wishlist
-        messages.info(request, 'The book was added to your temporary wishlist')
+        success = True
     else:
-        messages.info(request, 'The book is already in your temporary wishlist')
+        success = False
     
+    return JsonResponse({'success': success})
+
+
+def remove_from_wishlist(request, book_id):
+    wishlist = request.session.get('wishlist', [])
+    
+    if book_id in wishlist:
+        wishlist.remove(book_id)
+        request.session['wishlist'] = wishlist
+        success = True
+    else:
+        success = False
+
     return HttpResponseRedirect(reverse('wishlist'))
+
 
 def wishlist_view(request):
     wishlist = request.session.get('wishlist', [])
     books = Book.objects.filter(pk__in=wishlist)
-    return render(request, 'wishlist.html', {'books': books})
+    return render(request, 'wishlist.html', {'wishlist': books})
