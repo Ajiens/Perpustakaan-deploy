42e5f3714d968a8907307e46dc840226f8edefad
prasetyoadii <prasetyo.adi21@ui.ac.id>
2023-10-29 21:50:58 +0700

    mengerjakan form :)

--- pinjam_buku/urls.py
+++ pinjam_buku/urls.py
@@ -7,6 +7,10 @@ app_name = 'pinjam_buku'
 urlpatterns =[
     path("", show_borrow, name='pinjam_buku_show'),
     path('pinjam/<int:id>/', pinjam_buku_request, name='pinjam'),
-    path("pinjam_buku/", pinjam_buku, name="pinjam_buku"),
-    path("kembalikan/<int:borrow_id>/", kembalikan_buku, name="kembalikan_buku"),
+    path("pinjam_buku/", pinjam_buku_ajax, name="pinjam_buku"),
+    path("kembalikan/<int:id>/", kembalikan_buku, name="kembalikan_buku"),
+    path('get_buku_json/<int:id>/', get_buku_json_by_id, name='get_buku_json_by_id'),
+    path('available_books_json/', available_books_json, name='available_books_json'),
+    path('get_objek_json/<int:id>/', get_objek_by_id, name='get_objek_by_id'),
+    path('get_borrowed_books_json/', get_borrowed_buku_json, name='get_borrowed_buku_json'),
 ]
--- pinjam_buku/views.py
+++ pinjam_buku/views.py
@@ -1,31 +1,27 @@
 import datetime
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, JsonResponse
 from django.core import serializers
 from pinjam_buku.models import Borrow
 from book.models import Book
-
-def available_books(request):
-    available_books = Book.objects.filter(is_available=True)
-    return render(request, 'available_books.html', {'available_books': available_books})
-
-def get_borrowed_buku_json(request):
-    borrowed_books = Borrow.objects.filter(user=request.user, return_date__isnull=True)
-    return HttpResponse(serializers.serialize('json', borrowed_books))
+from django.views.decorators.csrf import csrf_exempt
 
 def show_borrow(request):
     user = request.user
     borrowed_books = Borrow.objects.filter(user=user, return_date__isnull=True)
     return render(request, 'main_pinjam.html', {'borrowed_books': borrowed_books})
 
-def pinjam_buku(request, book_id):
+@csrf_exempt
+def pinjam_buku_ajax(request):
+    print("bencana")
     if request.method == 'POST':
-        book = get_object_or_404(Book, pk=book_id)
+        book = get_object_or_404(Book, pk=request.POST.get("bookId"))
         telephone_number = request.POST.get("telephone_number")
         email_address = request.POST.get("email_address")
         duration_days = request.POST.get("duration_days")
-        if book.is_available and telephone_number and email_address and duration_days:
+
+        if book.is_available:
             borrow_date = datetime.date.today()  # Tanggal peminjaman
             duration_days = int(duration_days)  # Mengubah durasi peminjaman menjadi integer
             return_date = borrow_date + datetime.timedelta(days=duration_days)  # Menghitung tanggal pengembalian
@@ -34,31 +30,37 @@ def pinjam_buku(request, book_id):
 
             book.is_available = False
             book.save()
-            return redirect('main:show_main')
-        else:
-            return JsonResponse({'status': 'error', 'message': 'Buku tidak tersedia untuk dipinjam'}, status=400)
-    return render (request,'pinjam_buku.html')
+            return HttpResponse(b"CREATED", status=201)
+    
+    return HttpResponseNotFound()
 
-
+@csrf_exempt
 def kembalikan_buku(request, borrow_id):
-    borrow = get_object_or_404(Borrow, pk=borrow_id)
-    book = borrow.book
-    book.is_available = True
-    book.save()
-    borrow.return_date = datetime.date.today()
-    borrow.save()
-
-    return HttpResponseRedirect(reverse('pinjam_buku:show_borrow'))
-
+    if request.method == 'POST':
+        buku_dikembalikan = Book.objects.get(pk=id)
+        buku_dikembalikan.is_available= True
+        buku_dikembalikan.save()
+        objekPeminjaman = Borrow.objects.filter(buku=buku_dikembalikan).filter(user=request.user)
+        objekPeminjaman.first().delete()
+        return HttpResponse(b"DIHAPUS", status = 201)
+    return HttpResponseNotFound()    
 
 def pinjam_buku_request(request, id):
     book = Book.objects.values().get(pk=id)
     return render(request, 'pinjam_buku.html', {'book':book})
 
-def get_borrow_json_by_id(request, id):
-    book = Borrow.objects.values().get(pk=id)
+def get_buku_json_by_id(request, id):
+    book = Book.objects.values().get(pk=id)
     return JsonResponse(book, safe=False)
 
-def show_json(request):
-    data = Borrow.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+def available_books_json(request):
+    available_books = Book.objects.filter(is_available=True)
+    return HttpResponse(serializers.serialize('json', available_books))
+
+def get_objek_by_id(request, id):
+    cariPinjaman = Borrow.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize('json', cariPinjaman))
+
+def get_borrowed_buku_json(request):
+    borrowed_books = Borrow.objects.filter(user=request.user, return_date__isnull=True)
+    return HttpResponse(serializers.serialize('json', borrowed_books))
