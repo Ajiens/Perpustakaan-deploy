b34afe50564c196d8a0e595205327142bc87ffd3
nazwaikhsan <nazwa.arifah@ui.ac.id>
2023-10-29 13:08:20 +0700

    fixing views.py in add_member app

--- add_member/views.py
+++ add_member/views.py
@@ -1,15 +1,71 @@
-from django.shortcuts import render, redirect
-from .forms import MemberForm
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from .models import Member
+from book.models import Book, Borrow, Wishlist, Review
 
-def add_member(request):
-    if request.method == 'POST':
-        form = MemberForm(request.POST)
-        if form.is_valid():
-            member = form.save(commit=False)
-            member.save()
-            # Logic buat promo klo pelanggannya member
-            return redirect('nama_app:home')  # Ganti 'nama_app:home' dengan URL halaman setelah mendaftar member
-    else:
-        form = MemberForm()
-    return render(request, 'add_member.html', {'form': form})
+@login_required
+def karyawan_view(request):
+    try:
+        member = Member.objects.get(username=request.user.username, role='employee')
+        pelanggan_list = Member.objects.filter(role='customer')
+        history_peminjaman = Borrow.objects.all()
+        daftar_wishlist = Wishlist.objects.all()
+
+        context = {
+            'pelanggan_list': pelanggan_list,
+            'history_peminjaman': history_peminjaman,
+            'daftar_wishlist': daftar_wishlist,
+        }
+
+        return render(request, 'add_member/karyawan.html', context)
+    except Member.DoesNotExist:
+        # Pengguna bukan karyawan atau tidak ada objek Member dengan peran 'employee'
+        return render(request, 'add_member/error.html', {'error_message': 'Anda tidak memiliki akses sebagai karyawan.'})
+
+@login_required
+def pelanggan_view(request):
+    try:
+        member = Member.objects.get(username=request.user.username, role='customer')
+        katalog_buku = Book.objects.all()
+
+        if request.method == 'POST':
+            action = request.POST.get('action')
+            book_id = request.POST.get('book_id')
+            book = Book.objects.get(pk=book_id)
+
+            if action == 'pinjam':
+                if book.is_available:
+                    borrow = Borrow(book=book, user=request.user, telephone_number=member.telephone_number, email_address=member.email, duration_days=7)
+                    borrow.save()
+
+                    book.is_available = False
+                    book.save()
+
+                    messages.success(request, f"Buku {book.title} berhasil dipinjam.")
+                else:
+                    messages.error(request, f"Buku {book.title} tidak tersedia untuk dipinjam.")
+
+            elif action == 'wishlist':
+                wishlist = Wishlist(book=book, user=request.user)
+                wishlist.save()
+
+                messages.success(request, f"Buku {book.title} ditambahkan ke wishlist.")
+
+            elif action == 'review':
+                rating = request.POST.get('rating')
+                comment = request.POST.get('comment')
+
+                review = Review(book=book, user=request.user, rating=rating, comment=comment)
+                review.save()
+
+                messages.success(request, "Review Anda telah ditambahkan.")
+            return redirect('pelanggan_view')
+
+        context = {
+            'katalog_buku': katalog_buku,
+        }
+
+        return render(request, 'add_member/pelanggan.html', context)
+    except Member.DoesNotExist:
+        return render(request, 'add_member/error.html', {'error_message': 'Anda tidak memiliki akses sebagai pelanggan.'})
