7f703d97ac073e1901624fc0b788cfd75bf1faf9
nazwaikhsan <nazwa.arifah@ui.ac.id>
2023-10-29 09:30:05 +0700

    raw design

--- add_member/models.py
+++ add_member/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    role_choices = [
+        ('customer', 'Pelanggan'),
+        ('employee', 'Karyawan'),
+    ]
+    role = models.CharField(max_length=10, choices=role_choices)
+    status_choices = [
+        ('member', 'Member'),
+        ('non_member', 'Non-Member'),
+    ]
+    status = models.CharField(max_length=10, choices=status_choices)
+
+    def __str__(self):
+        return self.username
--- add_member/views.py
+++ add_member/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MemberForm
+from .models import Member
 
-# Create your views here.
+def add_member(request):
+    if request.method == 'POST':
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            member = form.save(commit=False)
+            member.save()
+            # Logic buat promo klo pelanggannya member
+            return redirect('nama_app:home')  # Ganti 'nama_app:home' dengan URL halaman setelah mendaftar member
+    else:
+        form = MemberForm()
+    return render(request, 'add_member.html', {'form': form})
